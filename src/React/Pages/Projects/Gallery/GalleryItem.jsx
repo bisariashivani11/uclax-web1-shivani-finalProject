95b1a4dc536d85ead6b0f64b770815e71d2aa982
shivani <shivani.bisaria11@gmail.com>
2021-03-08 12:07:02 -0800

    Progress on Services: Lightboxes

--- src/React/Pages/Projects/Gallery/GalleryItem.jsx
+++ src/React/Pages/Projects/Gallery/GalleryItem.jsx
@@ -21,9 +21,9 @@ const GalleryItem = ({ item }) => {
 
     return (
         <GalleryItemStyled className='GalleryItem'>
-            <div onClick={ handleOnShow }>
+            <div className='piece' onClick={ handleOnShow }>
                 <img src={ item.image } alt={ item.title }/>
-                {/* <h2>{ item.title }</h2> */}
+                <h2>{ item.title }</h2>
                 <h3>{ item.place }</h3>
                 <h4>{ item.category }</h4>
             </div>
@@ -32,7 +32,10 @@ const GalleryItem = ({ item }) => {
                 show={ showLightbox }
                 onHide={ handleOnHide }
             >
-                { item.title }
+                <img src={ item.image } alt={ item.title }/>
+                <h2>{ item.title }</h2>
+                <h3>{ item.place }</h3>
+                <h4>{ item.category }</h4>
             </Lightbox>
 
         </GalleryItemStyled>
@@ -45,29 +48,40 @@ const GalleryItemStyled = styled.div`
     position: relative;
     margin: 10px;
 
-    h2 {
-        position: absolute;
-        bottom: 0px; left:0px; right:0px;
-        background-color: #fff;
-        margin: 0px;
-        padding: 5px;
+    .piece { 
+        h2.galleryH2 {
+            position: absolute;
+            bottom: 0px; left:0px; right:0px;
+            background-color: #fff;
+            margin: 0px;
+            padding: 5px;
+        }
+
+        h3.galleryH3 {
+            position: absolute;
+            bottom: 0px; left: 0px; right: 0px;
+            text-align: right;
+            margin: 0px;
+            padding: 5px;
+        }
+
+        h4.galleryH4 {
+            position: absolute;
+            top: 0px; left:0px; right:0px;
+            text-align: right;
+            background-color: #fff;
+            margin: 0px;
+            padding: 5px;
+        }
     }
 
-    h3 {
-        position: absolute;
-        bottom: 0px; left: 0px; right: 0px;
-        text-align: right;
-        margin: 0px;
-        padding: 5px;
-    }
-
-    h4 {
-        position: absolute;
-        top: 0px; left:0px; right:0px;
-        text-align: right;
-        background-color: #fff;
-        margin: 0px;
-        padding: 5px;
+    .Lightbox {
+        h2 {
+            background-color: #5B5FA1;
+            color: white;
+            padding: 10px;
+            margin: 0px;
+        }
     }
     
 `;
--- src/React/Shared/Lightbox/Light.jsx
+++ src/React/Shared/Lightbox/Light.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+    faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+
+
 const Light = ({children}) => {
     return (
         <LightStyled className='Light'>
+            <button className='close'>
+                <FontAwesomeIcon icon={ faTimes } />
+            </button>
             { children }
         </LightStyled>
     );
@@ -12,7 +21,36 @@ const Light = ({children}) => {
 export default Light;
 
 const LightStyled = styled.div`
+
     background-color: white;
     padding: 20px;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    transform: translate(-50%, -50%);
+
+    width: 500px;
+
+    button.close {
+        background-color: #fff;
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+
+        margin: 0px;
+        padding: 0px;
+        border: none;
+
+        box-shadow: 3px 0px 10px rgba(0,0,0,.5);
+
+        cursor: pointer;
+
+        position: absolute;
+        right: -10px;
+        top: -10px;
+
+    }
     
 `;
