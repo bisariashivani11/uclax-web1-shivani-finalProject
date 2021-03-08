ef7f39161f02a950f19af67c5d1af8d3bb418df6
shivani <shivani.bisaria11@gmail.com>
2021-03-08 13:45:37 -0800

    Working on contact form and controlled inputs

---
+++ src/React/Shared/UniversalForm/FieldGroup/Fields/Input.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Input = ({theField, onChange}) => {
+
+    return (
+        <InputStyled
+            className='Input'
+
+            id={ theField.id }
+            value={ theField.value }
+
+            onChange={ onChange }
+        />
+    );
+}
+
+export default Input;
+
+const InputStyled = styled.input`
+    
+`;
---
+++ src/React/Shared/UniversalForm/FieldGroup/Fields/Textarea.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Textarea = ({theField, onChange}) => {
+
+    return (
+        <TextareaStyled
+            className='Textarea'
+
+            id={ theField.id }
+            value={ theField.value }
+
+            onChange={ onChange }
+        />
+    );
+}
+
+export default Textarea;
+
+const TextareaStyled = styled.textarea`
+    
+`;
